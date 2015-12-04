5c772d79b146b21093d8035fa91a164792639bd2
nocohen <nocohen@gmail.com>
2015-12-03 23:39:41 -0800

    seems important to save

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-sudo  MAILGUN_KEY='key-443b528ce49d61cf5a7281c7c3be36a8' MAILGUN_URL='https://api.mailgun.net/v3/mailgun.dxxd.net/messages' MAILGUN_RECIPIENT='ryandetzel@gmail.com' python server.py
+sudo  MAILGUN_KEY='key-c213fdfc32a6d34e24b857643aebd34d', MAILGUN_URL='https://api.mailgun.net/v3/mailgun.dxxd.net/messages', MAILGUN_RECIPIENT='nocohen+coopmail@gmail.com' python /home/pi/NoahPrograms/CoopControl/serverNC.py
